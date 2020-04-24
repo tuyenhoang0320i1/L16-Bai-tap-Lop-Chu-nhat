dcb2ef60738292dfe6a17a7a66af468e5b40935c
tuyenhoang0320i1 <63198356+tuyenhoang0320i1@users.noreply.github.com>
2020-04-24 12:38:37 +0700

    update Ex

--- ClassRectangle.js
+++ ClassRectangle.js
@@ -1,28 +1,40 @@
-function Rectangle(width, height) {
-    this.width = width;
-    this.height=height;
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let c = canvas.getContext("2d");
+class Rectangle {
+    constructor(x, y, width, height) {
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
 
-    this.getPerimeter = function () {
-        return (width + height)/2;
-    };
-    this.getArea = function () {
+    }
+    getPerimeter() {
+        return (width+height)*2;
+    }
+    getArea() {
         return width * height;
-    };
-    this.setWidth = function (value) {
-        this.width = value;
-    };
-    this.setHeight = function (value) {
-        this.height = value;
+    }
+    setWidth(value) {
+        this.width=value;
+    }
+    setHeight(value) {
+        this.height=value;
+    }
+    drawRectangle(canvas) {
+        c.beginPath();
+        c.rect(this.x,this.y,this.width,this.height);
+        c.fillStyle = "blue";
+        c.fill();
     }
 }
+// let width = Math.random() * 200;
+// let height = Math.random() * 200;
+let x = Math.random() * innerWidth;
+let y = Math.random() * innerHeight;
+let rectangle = new Rectangle(x, y, 150, 100);
+rectangle.drawRectangle();
+// alert("Perimeter is: " + rectangle.getPerimeter());
+// alert("Area is: " + rectangle.getArea());
 
-let rectangle = new Rectangle(3,5);
-let perimeter = rectangle.getPerimeter();
-let area = rectangle.getArea();
-alert("perimeter: " + perimeter + "; area: " + area);
-
-rectangle.setWidth(2);
-rectangle.setHeight(3);
-perimeter = rectangle.getPerimeter();
-area = rectangle.getArea();
-alert("perimeter: " + perimeter + "; area: " + area);
